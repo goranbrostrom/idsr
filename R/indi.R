641811a212ab136dfb2732e99c2497a5d716d3c0
Göran Broström <goran.brostrom@umu.se>
2017-02-06 14:28:07 +0100

    first commit: docs of dada sets

---
+++ R/indi.R
@@ -0,0 +1,15 @@
+#' Chronicle file with one person (Id_I = 10000325)
+#'
+#' A training data set for debugging.
+#'
+#' @format A data frame with 12 rows and 7 columns
+#' \describe{
+#'    \item{Id_I}{Id number (character)}
+#'    \item{Type}{Variable names}
+#'    \item{Value}{Variable value (character)}
+#'    \item{Year}{Year (numeric)}
+#'    \item{Month}{Month (numeric)}
+#'    \item{Day}{Day (numeric)}
+#'    \item{DayFrac}{Fraction of a day (usually 0.02)}
+#' }
+"indi"
---
+++ R/varset.R
@@ -0,0 +1,11 @@
+#' VarSetup file
+#'
+#' A training data set for debugging.
+#'
+#' @format A data frame with 19 rows and 3 columns
+#' \describe{
+#'    \item{Type}{Id number (character)}
+#'    \item{Transition}{Variable names}
+#'    \item{Duration}{Variable value (character)}
+#' }
+"varset"
