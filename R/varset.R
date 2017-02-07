1ed3b58913ec87043deae4a86f8475bed824d35c
Göran Broström <goran.brostrom@umu.se>
2017-02-07 18:37:15 +0100

    new data set

--- R/varset.R
+++ R/varset.R
@@ -2,10 +2,14 @@
 #'
 #' A training data set for debugging.
 #'
-#' @format A data frame with 19 rows and 3 columns
+#' @format A data frame with 19 rows and 3 or 4 columns
 #' \describe{
 #'    \item{Type}{Id number (character)}
 #'    \item{Transition}{Variable names}
 #'    \item{Duration}{Variable value (character)}
+#'    \item{mode}{Optional; if present it gives the storage mode
+#'    for each variable. Possible values are "numeric",
+#'    "factor", "character", and "date". If not present, all variables
+#'    in the episodes data frame are stored in character mode.}
 #' }
 "varset"
