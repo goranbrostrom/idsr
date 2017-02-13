003a6c8d149fb0c3d6fa08ebf0821e7f84674a31
Göran Broström <goran.brostrom@umu.se>
2017-02-13 09:34:35 +0100

    version 0.1.1

---
+++ R/ddb.R
@@ -0,0 +1,23 @@
+#' INDIVIDUAL data  with 100 persons from DDB
+#'
+#' A sample data set.
+#'
+#' @format A data frame with 3181 rows and 18 columns
+#' \describe{
+#'    \item{Id}{Record ID (int)}
+#'    \item{Id_D}{Data base ID (int)}
+#'    \item{Id_I}{Individual ID number (int)}
+#'    \item{Source}{Name of register}
+#'    \item{Type}{Variable names}
+#'    \item{Value}{Variable value (character)}
+#'    \item{Day}{Day (numeric)}
+#'    \item{Month}{Month (numeric)}
+#'    \item{Year}{Year (numeric)}
+#'    \item{Start_day}{Start Day (numeric)}
+#'    \item{Start_month}{Start Month (numeric)}
+#'    \item{Start_year}{Start Year (numeric)}
+#'    \item{End_day}{End Day (numeric)}
+#'    \item{End_month}{End Month (numeric)}
+#'    \item{End_year}{End Year (numeric)}
+#'  }
+"ddb"
