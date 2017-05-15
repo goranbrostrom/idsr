6a328179ac1d5123a8072e6752c7705cc7df5dd3
Göran Broström <goran.brostrom@umu.se>
2017-05-15 12:42:31 +0200

    first commit

---
+++ R/episodes.R
@@ -0,0 +1,9 @@
+  #' Create an episodes file from a Chronicle and a VarSetup file.
+#'
+#' @export
+#' @param chron The Chronicle data frame.
+#' @param events The Events data frame.
+#' @return An episodes data frame.
+
+episodes <- function(chron, events){
+}  
