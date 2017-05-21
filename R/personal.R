eaa752a6051d8dfc99f371bbd77467b234e37ab1
Göran Broström <goran.brostrom@umu.se>
2017-05-21 23:00:47 +0200

    First release

---
+++ R/personal.R
@@ -0,0 +1,12 @@
+#' Personal file, 16 records
+#' 
+#' An example data set
+#' 
+#' @format A data frame with 16 rows and 4 columns
+#' \describe{
+#' \item{Id_I}{Id number}
+#' \item{Type}{Variable names}
+#' \item{Value}{Variable value}
+#' \item{Value_Id_C}{Link to context (should not be necessary)}
+#' }
+"personal"
