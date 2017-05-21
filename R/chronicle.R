2bbb0f0303f3dba600a6b8d463c45c4bec8619d0
Göran Broström <goran.brostrom@umu.se>
2017-05-21 23:00:32 +0200

    First release

---
+++ R/chronicle.R
@@ -0,0 +1,12 @@
+#' Chronicle file, 39 records
+#' 
+#' An example data set
+#' 
+#' @format A data frame with 39 rows and 4 columns
+#' \describe{
+#' \item{Id_I}{Id number}
+#' \item{Type}{Event names}
+#' \item{Value}{Description of event}
+#' \item{date}{Date of event}
+#' }
+"chronicle"
